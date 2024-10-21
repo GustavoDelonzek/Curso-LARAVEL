ebe5b861de10da09e7612f5197c43c6532d86900
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2024-10-21 17:42:43 -0300

    Criando migrations com relacionamentos diversos

--- super-gestao/database/migrations/2024_10_21_142213_alter_fornecedores_nova_coluna_site_com_after.php
+++ super-gestao/database/migrations/2024_10_21_142213_alter_fornecedores_nova_coluna_site_com_after.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 50);
-            $table->timestamps();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string("site",150)->after('nome')->nullable();
+            
         });
     }
 
@@ -23,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropColumn("site");
+            
+        });
     }
 };
